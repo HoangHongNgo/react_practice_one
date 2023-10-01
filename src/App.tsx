1537ec5b6466d01704ff8fd7ab1621a06b2d72de
HoangHongNgo <honghoangngo@gmail.com>
2023-10-01 18:14:57 +0700

    refactor: add type for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Page404 from "./pages/Page404/Page404";
-import React from "react";
 import Login from "./pages/Login/Login";
 
 const Home = React.lazy(() => import("./pages/Home/Home"));
 
-function App() {
+const App = (): JSX.Element => {
   const router = createBrowserRouter([
     {
       element: <Layout />,
@@ -25,6 +25,6 @@ function App() {
     },
   ]);
   return <RouterProvider router={router} />;
-}
+};
 
 export default App;
